5a310ca31790227a8ac2e53d3277713a180b09a4
harshit <harshitpareek893@gmail.com>
2024-08-19 23:01:53 +0530

    addedsecrettoenv

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken';
 import { User } from '@prisma/client';
 import { jwtToken } from '../type/type';
-const SECRET = 'harSHI132';
+import 'dotenv/config'
+
+const JWT_SECRET = process.env.JWT_SECRET || "";
 
 function createJWTToken(user: User) {
   if (!user) {
@@ -14,13 +16,13 @@ function createJWTToken(user: User) {
   };
 
   // Using HS256 algorithm with a simple secret
-  const token = jwt.sign(payload, SECRET);
+  const token = jwt.sign(payload, JWT_SECRET);
   return token ;
 }
 
 function jwtTokenService(token : string){
   try{
-  return jwt.verify(token, SECRET) as jwtToken;
+  return jwt.verify(token, JWT_SECRET) as jwtToken;
   }
   catch(error){
     return null;
