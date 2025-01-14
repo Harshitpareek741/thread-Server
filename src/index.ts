f086b1f79430991f680449ded0d63ab4fe625424
Harshit Pareek <139744660+Harshitpareek741@users.noreply.github.com>
2025-01-14 20:42:30 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import initStart from './app'
 import main from './clientdb';
 const Port = 8080 || process.env.port;
+import cors from 'cors';
 
 async function ServerStart(){
     const app = await initStart();
+    app.use(cors());
     app.listen(Port , ()=>{
         console.log(`Server Started on Port ${Port}`);
     })
